432ce228ab6b9b2a0ac5bdc4a93dd0e599f36f5c
merv-e <34655454+merv-e@users.noreply.github.com>
2023-05-26 13:02:21 +0200

    - Progressbar is created and basic styling is added. - additional styling features are added to buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Form1 from './components/Form1';
+import ProgressBar from './components/ProgressBar';
 
 function App() {
   return (
     <div className="App">
       <h1>Registration Form</h1>
+      <ProgressBar/>
       <Form1/>
     </div>
   );
---
+++ src/components/ProgressBar.js
@@ -0,0 +1,17 @@
+import styles from "./UI/ProgressBar.module.css";
+
+const ProgressBar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles["progress-container"]}>
+        <div className={styles.progress} id="progress"></div>
+        <div className={`${styles.circle} active? ${styles.active}`}>1</div>
+        <div className={styles.circle}>2</div>
+        <div className={styles.circle}>3</div>
+        <div className={styles.circle}>4</div>
+      </div>
+    </div>
+  );
+}
+
+export default ProgressBar
