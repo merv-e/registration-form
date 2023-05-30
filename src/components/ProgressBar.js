5e22b708fd3d281af8335c162e3004cc8d77a83f
merv-e <34655454+merv-e@users.noreply.github.com>
2023-05-30 13:43:07 +0200

    changes that are made yesterday include - some changes in the styles -  the way the page renders the progress bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -2,17 +2,52 @@ import styles from "./UI/ProgressBar.module.css";
 
 const ProgressBar = props => {
 
+  let currActive = 1;
+
+  const isActive = props.progress === currActive; 
+
+  console.log(isActive);
+
+  console.log(`current active: ${currActive}`);
+  console.log(typeof currActive);
+  console.log(`progress:  ${props.progress}`);
+  console.log(typeof props.progress);
+
+  const progressNum = [1, 2, 3, 4];
+
+
+
   return (
     <div className={styles.container}>
       <div className={styles["progress-container"]}>
+        {/*below,  when the second classname "&& styles["progress-width"]" is added, the progressbar becomes different and you see another line on the left side in the progress-bar. */}
+
+        {/* The line in between */}
+        <div className={styles.progress} id="progress"></div>
+
+        {/* {progressNum.map((num) => (
+          <div
+            key={num}
+            id={num}
+            className={`${styles.circle} ${num} === ${props.progress} ? ${styles.active}
+          `}
+          >
+            {num}
+          </div>
+        ))} */}
+
         <div className={styles.progress} id="progress"></div>
         <div className={`${styles.circle} active? ${styles.active}`}>1</div>
-        <div className={styles.circle}>2</div>
-        <div className={styles.circle}>3</div>
-        <div className={styles.circle}>4</div>
+        <div className={`${styles.circle} active? ${styles.active}`}>2</div>
+        <div className={`${styles.circle} active? ${styles.active}`}>3</div>
+        <div className={`${styles.circle} active? ${styles.active}`}>4</div>
       </div>
     </div>
   );
 }
 
 export default ProgressBar
+
+// classname "active" is going to change (will be removed in some divs and added in another)
+
+// width of the line classNAme : progress will be changed when pressed next or prev.  in between 1/3 :)
