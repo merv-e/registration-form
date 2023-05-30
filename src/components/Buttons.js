23adf19e7430b046c1d1c0c9f963ed79a8cb683b
merv-e <34655454+merv-e@users.noreply.github.com>
2023-05-30 17:31:42 +0200

    -some styling - some functionality buttons are conditionally  rendered based on which part of the form user is on.

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -4,19 +4,31 @@ const Buttons = props => {
 
   return (
     <div className="btn">
-      <button 
-       onClick={props.prev}
-       disabled={props.progress === 1}
-      >
-        Prev
-      </button>
-
-      <button
-        onClick={props.next}
-        disabled={props.progress === 4}
-      >
-        Next
-      </button>
+        <button 
+          type="text"
+          onClick={props.prev}
+          disabled={props.progress === 1}
+        >Prev
+        </button>
+    {
+      props.progress !== 4 
+      ? (
+          <button 
+            type="text"
+            onClick={props.next}
+            disabled={props.progress === 4}
+          >
+            Next
+          </button>
+      )
+      : (
+          <button
+            type="submit"
+            className="register-btn"
+          >Register   
+          </button>
+      )
+    }
     </div>
   );
 }
