7c496d1303d2b2be5215c4c60ad1ea1fb18cc697
merv-e <34655454+merv-e@users.noreply.github.com>
2023-06-01 12:53:30 +0200

    a file is created for user data.

---
+++ src/components/data.js
@@ -0,0 +1,17 @@
+const users = [{
+    id: {
+        id: "",
+        name: "",
+        surname: "",
+        country: "",
+        occupation: "",
+        gender: "",
+        language: "",
+        email_address: "",
+        phone_number: "",
+        website: "",
+        password: "",
+    },
+}];
+
+export default users
